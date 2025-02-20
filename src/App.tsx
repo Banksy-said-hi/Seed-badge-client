f47b195e24f8e199af04785c4959bb77dd7df434
rob1997 <robelgetnetrg@gmail.com>
2025-02-20 11:02:38 +0300

    added Google Auth and removed all other login options

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,75 @@
 import './App.css'
 import { useState, useEffect } from 'react';
-import { web3AuthInstance } from './web3Auth';
+import { web3AuthInstance, initialize } from './web3Auth';
+
+// Because of React's strict mode App() is called twice, but the web3AuthInstance should be initialized only once.
+// This variable is used to check if the web3AuthInstance is already initialized.
+// You can also remove <StrictMode> tags from main.tsx but I feel like it's there for a reason.
+// I hate javascript
+let initialized : boolean = false;
 
 function App() {
   const [isConnected, setIsConnected] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    web3AuthInstance.initModal()
-      .then(() => {
-        console.log("Web3Auth Initialized");
-        setIsConnected(web3AuthInstance.connected);
-      })
-      .catch((error) => {
-        console.error("Web3Auth initialization error " + error);
-      });
+    const init = async () => {
+      
+try{
+
+if(initialized)
+{
+  return;
+}
+
+initialized = true;
+
+web3AuthInstance.on("connected", () => {
+  setIsConnected(web3AuthInstance.connected);
+});
+
+  await initialize();
+
+  console.log("Web3Auth Initialized");
+
+  setIsLoading(false);
+}
+catch (error){
+  console.error("Web3Auth initialization error " + error);
+}
+
+      
+    };
+    init();
   }, []);
 
+  web3AuthInstance.on("connected", () => {
+    setIsConnected(web3AuthInstance.connected);
+  });
+
   const connect = async () => {
+    setIsLoading(true);
     try {
       const provider = await web3AuthInstance.connect();
       console.log("Connected " + provider);
       setIsConnected(true);
     } catch (error) {
       console.error("Connection Error " + error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   const logout = async () => {
+    setIsLoading(true);
     try {
       await web3AuthInstance.logout();
       console.log("Logged out");
       setIsConnected(false);
     } catch (error) {
       console.error("Logout Error " + error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -40,9 +77,13 @@ function App() {
     <div>
       <p>Check</p>
       {isConnected ? (
-        <button onClick={logout}>Logout</button>
+        <button onClick={logout} disabled={isLoading}>
+          {isLoading ? "..." : "Logout"}
+        </button>
       ) : (
-        <button onClick={connect}>Connect</button>
+        <button onClick={connect} disabled={isLoading}>
+          {isLoading ? "..." : "Connect"}
+        </button>
       )}
     </div>
   );
--- src/web3Auth.ts
+++ src/web3Auth.ts
@@ -1,5 +1,5 @@
 import { Web3Auth } from "@web3auth/modal";
-import { CHAIN_NAMESPACES, WEB3AUTH_NETWORK, UX_MODE} from "@web3auth/base";
+import { CHAIN_NAMESPACES, WEB3AUTH_NETWORK, UX_MODE, WALLET_ADAPTERS } from "@web3auth/base";
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
 import { AuthAdapter, WHITE_LABEL_THEME, WhiteLabelData } from "@web3auth/auth-adapter";
 
@@ -21,11 +21,11 @@ const chainConfig = {
   });
 
   const web3Auth = new Web3Auth({
-    clientId: "BPWmcBa2DZfRL6FYnz7ZdwiMKhUu32QdfWoR3LxjeW2vvruPCqZ0G-ojeniQyVaOg1jTZbEmOrZqRQWYFA21Opg", // Get your Client ID from the Web3Auth Dashboard
+    clientId: "BBciNB8_-ajaAbCTCcMlMFEkbRQn5l5C5BrYq25liwjWtm98X92ZmseHAE014DqyZxWcqi9pyR_0FkgEtZ4sQSY", // Get your Client ID from the Web3Auth Dashboard
     web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
     privateKeyProvider,
     uiConfig: {
-        appName: "W3A Heroes",
+        appName: "Klang",
         appUrl: "https://web3auth.io",
         logoLight: "https://web3auth.io/logo-light.png",
         logoDark: "https://web3auth.io/logo-dark.png",
@@ -37,9 +37,9 @@ const chainConfig = {
 
   const authAdapter = new AuthAdapter({
     adapterSettings: {
-      //clientId, // Optional - Provide only if you haven't provided it in the Web3Auth Instantiation Code
+      // clientId, // Optional - Provide only if you haven't provided it in the Web3Auth Instantiation Code
       // network: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET, // Optional - Provide only if you haven't provided it in the Web3Auth Instantiation Code
-      uxMode: UX_MODE.REDIRECT,
+      uxMode: UX_MODE.POPUP,
       whiteLabel: {
         appName: "W3A Heroes",
         appUrl: "https://web3auth.io",
@@ -52,6 +52,13 @@ const chainConfig = {
         } as WHITE_LABEL_THEME,
         useLogoLoader: true,
       } as WhiteLabelData,
+      loginConfig:{
+        google: {
+          verifier: "google_test", // Pass the Verifier name here
+          typeOfLogin: "google", // Pass on the login provider of the verifier you've created
+          clientId: "229577254612-np5j8aqqck2qf4aaa8ioo5qgl3j8nkq6.apps.googleusercontent.com", // Pass on the Google `Client ID` here
+        }, 
+      }
     },
     loginSettings: {
         mfaLevel: "optional"
@@ -60,5 +67,82 @@ const chainConfig = {
   });
 
   web3Auth.configureAdapter(authAdapter);
+
+  export async function initialize() {
+    await web3Auth.initModal({
+      modalConfig: {
+        [WALLET_ADAPTERS.AUTH]: {
+          label: "auth",
+          loginMethods: {
+            google: {
+              name: "Google",
+            },
+            twitter: {
+              name: 'twitter',
+              showOnModal: false,
+            },
+            facebook: {
+              name: 'facebook',
+              showOnModal: false,
+            },
+            github: {
+              name: 'github',
+              showOnModal: false,
+            },
+            reddit: {
+              name: 'reddit',
+              showOnModal: false,
+            },
+            discord: {
+              name: 'discord',
+              showOnModal: false,
+            },
+            twitch: {
+              name: 'twitch',
+              showOnModal: false,
+            },
+            apple: {
+              name: 'apple',
+              showOnModal: false,
+            },
+            line: {
+              name: 'line',
+              showOnModal: false,
+            },
+            kakao: {
+              name: 'kakao',
+              showOnModal: false,
+            },
+            linkedin: {
+              name: 'linkedin',
+              showOnModal: false,
+            },
+            weibo: {
+              name: 'weibo',
+              showOnModal: false,
+            },
+            wechat: {
+              name: 'wechat',
+              showOnModal: false,
+            },
+            farcaster:{
+              name: 'farcaster',
+              showOnModal: false,
+            },
+            email_passwordless: {
+              name: "email_passwordless",
+              showOnModal: false,
+            },
+            sms_passwordless: {
+              name: "sms_passwordless",
+              showOnModal: false,
+            },
+          },
+          // setting it to false will hide all social login methods from modal.
+          showOnModal: true,
+        },
+      },
+    });
+  }
 
   export const web3AuthInstance = web3Auth;
